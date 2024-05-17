c3678646bbee76ba6b12d84ad2e997a913d96181
erwanpia <erwan@wave.bzh>
2024-05-17 08:21:23 +0200

    introducing page mode : full / pad demo on /about

--- components/Base/Footer.vue
+++ components/Base/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class=" w-full  bg-blue-950 text-white border-y shadow text-center h-20 flex items-center justify-center">
     <div>
-       <a href="https://webmasterbulletin.net/"> nuxt tailwind template by <b>webmasterbulletin.net</b></a><br> with  &nbsp;<a href="/_tailwind/"><b>tailwind helper</b> (dev only)</a>
+       <a href="https://webmasterbulletin.net/2024/05/06/tailwind-responsive-menu-with-drop-down/"> nuxt tailwind template by <b>webmasterbulletin.net</b></a><br> with  &nbsp;<a href="/_tailwind/"><b>tailwind helper</b> (dev only)</a>
     </div>
   </footer>
 </template>
