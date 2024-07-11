44c1a2bd3e95179abceb7e6c3c28c262dc0d9c76
erwanpia <erwan@wave.bzh>
2024-07-11 17:42:06 +0200

    missin semi colon

--- components/Content/Text1.vue
+++ components/Content/Text1.vue
@@ -2,15 +2,3 @@
    <div class="contentText1"><span v-html="title"/><slot /></div>
 </template>
 <script setup>defineProps(['title'])</script>
-<style  scoped>
-*{ font-size:20px; }
-.contentText1 a:link ,
-.contentText1 a:visited,
-.contentText1 a:hover,
-.contentText1 a:active{   //TODO : fix , does not apply
-  text-decoration: underline;
-  color: red !important;
-  font-style: italic;
-  font-weight:bold;
-}
-</style>
