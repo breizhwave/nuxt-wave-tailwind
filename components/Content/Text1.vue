878929c328bc8cc0fe434bfa39b2b72538018d90
erwanpia <erwan@wave.bzh>
2024-07-11 17:40:07 +0200

    missin semi colon

--- components/Content/Text1.vue
+++ components/Content/Text1.vue
@@ -3,7 +3,7 @@
 </template>
 <script setup>defineProps(['title'])</script>
 <style  scoped>
-*{font-size:20px;}
+*{ font-size:20px; }
 .contentText1 a:link ,
 .contentText1 a:visited,
 .contentText1 a:hover,
